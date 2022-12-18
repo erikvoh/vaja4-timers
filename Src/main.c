3f892f09ea8b3e7382e7c6b211eea3fb3262bdb3
Erik Voh <erik.voh@gmail.com>
2022-12-18 11:11:04 +0100

    komentarji na obstoječo kodo

--- Src/main.c
+++ Src/main.c
@@ -31,11 +31,12 @@ void TIM3_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 
 /* Deklaracija spremenljivk */
+//take spremenljivke so no go, ker ne pomenijo ničesar. smiselno bi bilo kaj v smislu "led_blink" in "button_pressed"
 int flag=0;
 int flag1=0;
 
 void Prekinitev_konfiguracija (void){
-	//Konfiguracija tipke (PC13) kot vhod
+	//Konfiguracija tipke (PC13) kot vhod -> načeloma ne bi bilo potrebno, ker je input reset state za f4
 	CLEAR_BIT(GPIOC->MODER,1<<27);
 	CLEAR_BIT(GPIOC->MODER,1<<26);
 
@@ -62,25 +63,28 @@ int main(void)
 	SET_BIT(GPIOA->MODER,1<<10);//set PA5 as output
 
 	//TIM2 - postavitev PSCA registra in ARR registra, DIER register za prekinitev
-	WRITE_REG(TIM2->PSC,16000-1);
-	WRITE_REG(TIM2->ARR,1000-1);
+	//perioda timerja 1 s/1 Hz -> za utripanje led diode
+	WRITE_REG(TIM2->PSC,16000-1);	//prescaler - zniža clock timerja iz 16 MHz (HSI oscilator za core) na 1 kHz
+	WRITE_REG(TIM2->ARR,1000-1);	//auto-reload register - nastavi do koliko bo štel timer 0-999
 	SET_BIT(TIM2->DIER,1);//enable interrupt
 	NVIC_EnableIRQ(TIM2_IRQn);
 	SET_BIT(TIM2->CR1,TIM_CR1_CEN); //enable counter
-	//CLEAR_BIT(TIM2->CR1,TIM_CR1_CEN); //disable counter
 
 	//TIM3 - postavitev PSCA registra in ARR registra, DIER register za prekinitev
+	//perioda timerja 200 ms -> 5 Hz za debounce led diode
 	WRITE_REG(TIM3->PSC,16000-1);
-	WRITE_REG(TIM3->ARR,200-1);
+	WRITE_REG(TIM3->ARR,200-1);		//auto-reload register - nastavi do koliko bo štel timer 0-199
 	SET_BIT(TIM3->DIER,1);//enable interrupt
 	NVIC_EnableIRQ(TIM3_IRQn);
-	SET_BIT(TIM3->CR1,TIM_CR1_CEN); //enable counter
-	CLEAR_BIT(TIM3->CR1,TIM_CR1_CEN); //disable counter
+
+	//counterja se tukaj še ne sme omogočiti, ker ga je potrebno zagnati šele po pritisku gumba
 
 	//tipka
 	Prekinitev_konfiguracija();
-	NVIC_DisableIRQ(EXTI15_10_IRQn);// disable button
-	NVIC_EnableIRQ(EXTI15_10_IRQn);// enable button interrupt
+
+	//to ni potrebno onemogočiti in ponovno omogočiti, ker se naredi v funkciji "Prekinitev_konfiguracija"
+//	NVIC_DisableIRQ(EXTI15_10_IRQn);// disable button
+//	NVIC_EnableIRQ(EXTI15_10_IRQn);// enable button interrupt
 
 
 
